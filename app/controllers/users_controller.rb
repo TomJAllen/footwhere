5c8f27cee81d5cbaa1efee466de98667a17d2564
Tom Allen <tomallen_1@hotmail.co.uk>
2021-11-16 15:37:01 +0000

    stuff to check

--- app/controllers/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @booking = Booking.new
-    @bookings = Booking.all
-    @user_bookings = @bookings.where(user: current_user)
-  end
-end
