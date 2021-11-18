d08f288196fdb9f111b3f1787d93e3ea2f4dcb71
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-11-18 10:16:28 +0000

    cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_132234) do
+ActiveRecord::Schema.define(version: 2021_11_18_100540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2021_11_17_132234) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image_url"
     t.string "category"
     t.string "price"
     t.index ["user_id"], name: "index_shoes_on_user_id"
