8069d6315a8b09054d882524bcb9f9662081945a
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-11-18 20:14:32 +0000

    maps

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -11,6 +11,9 @@ class Shoe < ApplicationRecord
     using: {
       tsearch: { prefix: true }
     }
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
 
 # , inclusion: { in: ["hiking", "formal", "flippers", "sneakers", "wellies", "industrial", "climbing", "cycling", "sport"] }
