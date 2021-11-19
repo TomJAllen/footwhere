905789be716938b3e0ec54ff9444e21862c6de83
Oliver Barnes <oliverbarnes124@gmail.com>
2021-11-19 13:09:29 +0000

    addeds sdjbkfuhaildjdfbjndiajkl

--- db/migrate/20211118195736_rename_postcode_to_address_in_shoes.rb
+++
@@ -1,9 +0,0 @@
-class RenamePostcodeToAddressInShoes < ActiveRecord::Migration[6.0]
-  def up
-    rename_column :shoes, :postcode, :address
-  end
-
-  def down
-    rename_column :shoes, :address, :postcode
-  end
-end
